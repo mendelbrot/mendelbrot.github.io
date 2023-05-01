c7e0cd5b4b50df1fa53033e9a23aa1b2efaffcfa
Gregory Maxedon <gregory.maxedon@gmail.com>
2023-04-30 17:30:24 -0700

    finshed resume first iteration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './{_includes,_layouts,_pages,_posts}/**/*.{html,js,liquid}',
+    './{_includes,_layouts,_pages,_posts}/**/*.{html,js,liquid, md}',
     './.eleventy.js'
   ],
   theme: {
